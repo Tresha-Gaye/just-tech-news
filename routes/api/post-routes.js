403865fb2e3cf251236ab161e3e4ac912eb19a5c
Twigster <treshagaye@gmail.com>
2022-02-23 20:40:25 -0500

    completed feature/votes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,13 +1,21 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
-// get all users
+// get all user posts
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
       // Query configuration
-      attributes: ['id', 'post_url', 'title', 'created_at'],
-      order: [['created at', DESC]],   // orders by most recent post- latest posts will appear in response first
+      // update the `.findAll()` method's attributes to look like this
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      order: [['created_at', 'DESC']],   // orders by most recent post- latest posts will appear in response first
       include: [
           {
               model: User,
@@ -28,7 +36,13 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
           model: User,
@@ -64,6 +78,20 @@ router.post('/', (req, res) => {
       });
 });
 
+
+// PUT / api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+
+// update user post
 router.put('/:id', (req, res) => {
     Post.update(
       {
