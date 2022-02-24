aa0e77f54da0619eee851dfb2c63f3186407c8be
Twigster <treshagaye@gmail.com>
2022-02-23 22:40:51 -0500

    added comment model and routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all user posts
 router.get('/', (req, res) => {
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
     Post.findAll({
       // Query configuration
       // update the `.findAll()` method's attributes to look like this
+      order: [['created_at', 'DESC']],   // orders by most recent post- latest posts will appear in response first
       attributes: [
         'id',
         'post_url',
@@ -15,12 +16,20 @@ router.get('/', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-      order: [['created_at', 'DESC']],   // orders by most recent post- latest posts will appear in response first
       include: [
-          {
-              model: User,
-              attributes: ['username']
+        // include the comment model
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
           }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
       ]
     })
     .then(dbPostData => res.json(dbPostData))
@@ -44,6 +53,15 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+          // include the comment model
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+            }
+        },
         {
           model: User,
           attributes: ['username']
